fd4e6472f0c8c430d48bec3f8ba12b3e8b889e93
Michael Hunter <tahoemph@gmail.com>
2018-10-29 13:02:31 -0700

    What step is incorrect in adding yarn

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -137,9 +137,11 @@ fi
 echo "# install meteor
 RUN curl https://install.meteor.com/ | sh"
 
-#echo "# install yarn
-#RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
-#  && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
-#  && apt-get update && apt-get install yarn"
-echo "# install yarn
+echo "# install yarn key
+RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -"
+
+echo "# install yarn source list
+RUN echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list"
+
+echo "# install yarn apt
 RUN apt-get update && apt-get install yarn"
